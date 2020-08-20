ca13904a11db7f9a5c616252d5c618dc43821346
dhytest <dhytest@qq.com>
2020-08-20 09:05:24 +0800

    8.20 first

---
+++ SiliconValley.InformationSystem/SiliconValley.InformationSystem.Entity/Entity/AttendanceAnormaly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley.InformationSystem.Entity.Entity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    [Table("AttendanceAnormaly")]//考勤异常表
+    public partial class AttendanceAnormaly
+    {
+        [Key]
+        public int Id { get; set; }
+        public string EmployeeId { get; set; }//员工编号
+        public Nullable<int> AnormalyTypeId { get; set; }//考勤异常类型
+        public Nullable<System.DateTime> YearAndMonth { get; set; }//年月份
+
+    }
+}
